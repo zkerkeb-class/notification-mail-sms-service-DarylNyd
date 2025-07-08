38b69ca0d11384238fabee279884555b7de26f5d
DarylNyd <95940306+DarylNyd@users.noreply.github.com>
2025-07-08 14:49:25 +0200

    update

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 
 const express = require('express');
 const notificationRoutes = require('./routes/notificationRoutes');
+const promClient = require('prom-client');
+const register = promClient.register;
+promClient.collectDefaultMetrics({ register });
 
 const app = express();
 const PORT = process.env.PORT || 4003;
@@ -11,6 +14,11 @@ app.use(express.json());
 
 // Use notification routes
 app.use('/', notificationRoutes);
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 app.listen(PORT, () => {
   console.log(`Notification service running on port ${PORT}`);
